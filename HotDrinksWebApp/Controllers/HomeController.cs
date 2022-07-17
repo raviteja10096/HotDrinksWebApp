5e02a0dd0c9c32626fdf702962f10b53cf3ad6fc
raviteja <tholupunoori.raviteja@gmail.com>
2022-07-17 20:55:31 +0530

    Commit
    Initial Commit

---
+++ HotDrinksWebApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HotDrinksWebApp.Models;
+using System.Speech.Synthesis;
+
+namespace HotDrinksWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Configure the audio output.   
+            synth.SetOutputToDefaultAudioDevice();
+
+            // Speak a string.  
+            synth.Speak("This example demonstrates a basic use of Speech Synthesizer");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HotDrinksWebApp/Models/RecipesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace HotDrinksWebApp.Models
+{
+    public class RecipesModel
+    {
+        public string LemonTea { get; set; }
+        public string Coffee { get; set; }
+        public string Chocolate { get; set; }
+
+        public RecipesModel(string lemontea, string coffee,string chocoalte)
+        {
+            this.LemonTea = lemontea;
+            this.Coffee = coffee;
+            this.Chocolate = chocoalte;
+        }
+
+    }
+}
